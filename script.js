f4e1627ca46e56200ed8b6ee16b60af30718b11b
Andrew <tandrej768@gmail.com>
2022-07-10 23:23:26 +0300

    Version 1.0.0

---
+++ script.js
@@ -0,0 +1,169 @@
+const selectedDateElement = document.querySelector('.selected-date');
+const monthElement = document.querySelector('.mth');
+const nextMonthElement = document.querySelector('.next-mth');
+const prevMonthElement = document.querySelector('.prev-mth');
+const daysElement = document.querySelector('.days');
+const weekDays = document.querySelector('.weekDays');
+
+const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+const weekDayNames = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+
+
+let date = new Date();
+let day = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+
+let selectedDate = new Date(date);
+let selectedDay = day;
+let selectedMonth = month;
+let selectedYear = year;
+
+monthElement.textContent = months[month] + ' ' + year;
+
+selectedDateElement.value = formatDate(date);
+selectedDateElement.dataset.value = selectedDate;
+for (let dayName of weekDayNames) {
+  weekDays.innerHTML += `<div>${dayName}</div>`
+}
+
+populateDates();
+
+// EVENT LISTENERS
+selectedDateElement.addEventListener('input', handleChange);
+nextMonthElement.addEventListener('click', goToNextMonth);
+prevMonthElement.addEventListener('click', goToPrevMonth);
+
+// FUNCTIONS
+function goToNextMonth() {
+  month++;
+  if (month === 12) {
+    month = 0;
+    year++;
+    date.setFullYear(year);
+  }
+  date.setMonth(month);
+  monthElement.textContent = months[month] + ' ' + year;
+  selectedDateElement.value = formatDate(selectedDate);
+  populateDates();
+}
+
+function goToPrevMonth() {
+  month--;
+  if (month === -1) {
+    month = 11;
+    year--;
+    date.setFullYear(year);
+  }
+  date.setMonth(month);
+  monthElement.textContent = months[month] + ' ' + year;
+  selectedDateElement.value = formatDate(selectedDate);
+  populateDates();
+}
+
+function populateDates() {
+  daysElement.innerHTML = '';
+  day = date.getDate();
+  month = date.getMonth();
+  year = date.getFullYear();
+
+  selectedDay = selectedDate.getDate();
+  selectedMonth = selectedDate.getMonth();
+  selectedYear = selectedDate.getFullYear();
+
+  let amount_days = new Date(year, month + 1, 0).getDate();
+  let curMonthFirstDayIndex = getLocalDay(new Date(year, month, 1));
+  let lastDayMonthBefore = new Date(year, month, 0).getDate();
+  let curMonthEndDayIndex = getLocalDay(new Date(year, month + 1, 0));
+  let firstDayNextMonth = 1;
+
+
+  for (let i = curMonthFirstDayIndex - 1; i >= 1; i--) {
+    const dayElement = document.createElement('div');
+    dayElement.classList.add('day', 'prev');
+    dayElement.textContent = String(lastDayMonthBefore - i + 1);
+    daysElement.appendChild(dayElement);
+
+    dayElement.addEventListener('click', function () {
+      selectedDate = new Date(year, month - 1, lastDayMonthBefore - i + 1);
+      selectedDateElement.value = formatDate(selectedDate);
+    });
+
+    dayElement.addEventListener('click', goToPrevMonth);
+  }
+
+  for (let i = 0; i < amount_days; i++) {
+    const day_element = document.createElement('div');
+    day_element.classList.add('day');
+    day_element.textContent = String(i + 1);
+
+    if (selectedDay === (i + 1) && selectedYear === year && selectedMonth === month) {
+      day_element.classList.add('selected');
+    }
+
+    day_element.addEventListener('click', function () {
+      selectedDate = new Date(year, month, i + 1);
+      selectedDateElement.value = formatDate(selectedDate);
+      populateDates();
+    });
+
+    daysElement.appendChild(day_element);
+  }
+
+  for (let i = curMonthEndDayIndex + 1; i <= 7; i++) {
+    const dayElement = document.createElement('div');
+    const dayNumber = firstDayNextMonth;
+    dayElement.classList.add('day', 'next');
+    dayElement.textContent = String(firstDayNextMonth++);
+    daysElement.appendChild(dayElement);
+
+    dayElement.addEventListener('click', function () {
+      selectedDate = new Date(year, month + 1, dayNumber);
+      selectedDateElement.value = formatDate(selectedDate);
+    });
+
+    dayElement.addEventListener('click', goToNextMonth);
+  }
+}
+
+function handleChange(e) {
+  let input = e.target.value;
+  let testRegExr = /^(\d{2}).(\d{2}).(\d{4})$/;
+  let test = input.match(testRegExr);
+
+  if (test) {
+    const [, inpDay, inpMonth, inpYear] = test;
+    date = new Date(inpYear + '-' + inpMonth + '-' + inpDay);
+    if (date.toString() === 'Invalid Date') return;
+    selectedDate = new Date(date);
+    monthElement.textContent = months[date.getMonth()] + ' ' + date.getFullYear();
+    populateDates();
+  }
+}
+
+function getLocalDay(date) {
+
+  let day = date.getDay();
+
+  if (day === 0) {
+    day = 7;
+  }
+
+  return day;
+}
+
+function formatDate(d) {
+  let day = d.getDate();
+  if (day < 10) {
+    day = '0' + day;
+  }
+
+  let month = d.getMonth() + 1;
+  if (month < 10) {
+    month = '0' + month;
+  }
+
+  let year = d.getFullYear();
+
+  return day + '.' + month + '.' + year;
+}
